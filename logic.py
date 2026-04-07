b39112dca5f5692fb3dd39e1879d4d8a03398e8a
Jarvis <Jarvis@starkindustries.com>
2026-04-08 01:47:04 +0530

    Added function to fetch the filesystem and convert it into littlefs object. ( Confirmed test done that reads filesystem from ESP32 and correctly shows files inside it)

--- logic.py
+++ logic.py
@@ -2,19 +2,21 @@ from enum import Enum
 from dataclasses import dataclass
 from struct import unpack
 
+from esptool import ESPLoader
+from littlefs import LittleFS, UserContext
 from serial.tools import list_ports
 from serial.tools.list_ports_common import ListPortInfo
 
 from esptool.cmds import (
-attach_flash,
-read_flash,
-reset_chip,
-run_stub,
+    attach_flash,
+    read_flash,
+    reset_chip,
+    run_stub,
 )
 
 from esptool.targets import ESP32ROM
 
-@dataclass
+@dataclass(frozen=True)
 class BaseType:
     display_name: str
     command_value: str
@@ -27,18 +29,30 @@ class BoardType(BaseType, Enum):
     # ESP32_C3 = ("ESP32 C3", "esp32c3")
     # ESP32_C6 = ("ESP32 C6", "esp32c6")
 
-class FilesystemType(BaseType, Enum):
-#   Format : (Display Name, Command value)
-    LITTLEFS = ("LittleFS", "lfs")
-    # SPIFFS   = ("SPIFFS",   "spiffs")
-    # FATFS    = ("FatFS",    "fat")
-
 class BaudrateType(BaseType, Enum):
 #   Format : (Display Name, Command value)
     FAST    = ("921600",  921600)
     STANDARD = ("460800", 460800)
     LEGACY   = ("115200", 115200)
 
+class FilesystemType(BaseType, Enum):
+#   Format : (Display Name, Command value)
+    LITTLEFS = ("LittleFS", "lfs")
+    SPIFFS   = ("SPIFFS",   "spiffs")
+    FATFS    = ("FatFS",    "fat")
+
+    @property
+    def as_subtype(self):
+        """
+        Used when we want to compare FilesystemType variable to PartitionSubType variable
+        example usage in read_filesystem function
+        """
+        mapping = {
+            FilesystemType.LITTLEFS: PartitionSubType.LITTLEFS,
+            FilesystemType.SPIFFS: PartitionSubType.SPIFFS,
+            FilesystemType.FATFS: PartitionSubType.FATFS
+        }
+        return mapping.get(self, PartitionSubType.INVALID)
 
 @dataclass
 class ESPConfigType:
@@ -61,7 +75,7 @@ class PartitionSubType(Enum):
     SPIFFS = 0x82
     LITTLEFS = 0x83
 
-    # To be used when the partition sub type is not one of the relevant ones
+    # To be used when the partition subtype is not one of the relevant ones
     INVALID = 0xFF
 
 @dataclass
@@ -83,7 +97,6 @@ class PartitionTableEntry:
     # This is not related to any instance, but just a symbolic constant
     DEFAULT_MAGIC: int = 0x50AA
 
-
 def serial_port_enumerator(detailed: bool = False):
     """
     Gets a list of current serial ports
@@ -100,7 +113,7 @@ def serial_port_enumerator(detailed: bool = False):
 
 def parse_raw_partition_table_entry(raw_entry: bytes) -> PartitionTableEntry:
     """
-    Parses a single chunk of 32 bytes into a PartitionTableEntry object.
+    Parses a single chunk of 32 bytes into a PartitionTableEntry object.\n
     NOTE : values are **little endian**
     :return: PartitionTableEntry
     """
@@ -110,33 +123,25 @@ def parse_raw_partition_table_entry(raw_entry: bytes) -> PartitionTableEntry:
     magic, type_t, subtype, offset, size, label, flags = unpack("<HBBII16sI", raw_entry)
 
     type_t = PartitionType(type_t)
-
     try:
         subtype = PartitionSubType(subtype)
     except ValueError:
         subtype = PartitionSubType.INVALID
-
     label = label.rstrip(b'\x00').decode('utf-8')
 
     return PartitionTableEntry(type_t, subtype, offset, size, label, flags)
 
-def get_partition_table(config :ESPConfigType, offset: int):
+def get_partition_table(esp :ESPLoader, offset: int):
     """
     Reads partition table from the given offset
-    :param config: Information about the target ESP needed for connecting over serial
+    :param esp: The ESPLoader object to be used
     :param offset: The offset in memory where the partition table is located
     :return: a list of PartitionTableEntry
     """
-    raw_data = None
+
     partition_table: list[PartitionTableEntry] = []
 
-    if config.board == BoardType.ESP32:
-        with ESP32ROM(config.port) as esp:
-            esp.connect()  # Connect to the ESP chip, needed when ESP32ROM is instantiated directly
-            esp = run_stub(esp)  # Run the stub loader (optional)
-            attach_flash(esp)  # Attach the flash memory chip, required for flash operations
-            raw_data = read_flash(esp, offset, 0x1000)  # 0x1000 is the size of partition table including the MD5 checksum
-            reset_chip(esp, "hard-reset")  # Reset the board
+    raw_data = read_flash(esp, offset, 0x1000)  # 0x1000 is the size of partition table including the MD5 checksum
 
     for i in range(0,len(raw_data),32):
         magic = unpack("<H", raw_data[i:i+2])[0]
@@ -147,7 +152,73 @@ def get_partition_table(config :ESPConfigType, offset: int):
 
     return partition_table
 
-def get_filesyste_data():
-    
 
-get_partition_table(ESPConfigType(BoardType.ESP32, BaudrateType.FAST, "COM5"), 0x8000)
+def get_filesyste_raw_data(esp: ESPLoader, offset: int, size: int) -> bytes:
+    """
+    Simple function to run a query and fetch filesystem raw data from memory
+    Exists as a seperate function since it might get called from somewhere else
+    :param esp: The ESPLoader object to be used
+    :param offset: The offset of filesystem partition in memory
+    :param size: The size of filesystem in memory
+    :return:
+    """
+    raw_data = read_flash(esp, offset, size)
+    return raw_data
+
+
+def get_filesystem(config: ESPConfigType, partition_offset: int, fstype: FilesystemType, legacy: bool = True):
+    """
+    Function that can be called directly from GUI, it will fetch the full filesystem
+    and return the filesystem to GUI to be shown. This function handles calling other
+    functions and figure out filesystem offset with the partition table. Then it will
+    call the read raw filesystem function and convert it into appropriate littleFS object. \n
+    The legacy reasoning -
+    Filesystems flashed with arduino IDE flash the littleFS filesystem with SPIFFS flag
+    (should have been 0x83 but they flash it with 0x82, even though the underlying filesystem
+    is 0x83. SEtting the legacy parameter to True means, read the filesystem data and interpret it
+    as littleFS even if the filesystem label says SPIFFS)
+
+    :param config: Configuration of ESP32 to connect to
+    :param partition_offset: The offset of partition table in memory
+    :param fstype: The type of filesystem
+    :param legacy: Set to True to force interpret the filesystem as littleFS (commonly required when the board is flashed with arduino framework)
+    :return: probably a littleFS object, not sure yet
+    """
+    if config.board == BoardType.ESP32:
+        with ESP32ROM(config.port) as esp:
+            esp.connect()  # Connect to the ESP chip, needed when ESP32ROM is instantiated directly
+            esp = run_stub(esp)  # Run the stub loader (optional)
+            attach_flash(esp)  # Attach the flash memory chip, required for flash operations
+            esp.change_baud(config.baudrate.command_value) #upgrade speed from 115200 baud to faster one
+
+            # Fetch partition table and figure out filesystem offset
+            table = get_partition_table(esp, partition_offset)
+            fs_offset = None
+            fs_size = None
+            for entry in table:
+                if entry.type == PartitionType.DATA:
+                    if entry.subtype != PartitionSubType.INVALID:
+                        if entry.subtype == fstype.as_subtype or ( (fstype == FilesystemType.LITTLEFS) and (entry.subtype == PartitionSubType.SPIFFS) and legacy):
+                            fs_offset = entry.offset
+                            fs_size = entry.size
+                            break
+
+            assert fs_offset is not None and fs_size is not None
+
+            filesystem_raw_data = get_filesyste_raw_data(esp, fs_offset, fs_size)
+            filesystem_raw_data = bytearray(filesystem_raw_data)
+
+            fs = LittleFS(
+                context=UserContext(buffer=filesystem_raw_data),
+                block_size = 4096,
+                block_count = fs_size//4096,
+                read_size=16,
+                prog_size=16,
+            )
+
+            for root, dirs, files in fs.walk("."):
+                print(f"root {root} dirs {dirs} files {files}")
+
+            reset_chip(esp, "hard-reset")  # Reset the board
+
+get_filesystem(ESPConfigType(BoardType.ESP32, BaudrateType.FAST, "COM5"), 0x8000, FilesystemType.LITTLEFS)
