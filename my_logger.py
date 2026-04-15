ac3f3fe08dd136575424f78dce11e170e5b73d27
Jarvis <Jarvis@starkindustries.com>
2026-04-16 01:13:30 +0530

    Made the logger multiprocessing compatible. So if we pass in a queue to the logger , it will push messages in that queue , otherwise it simply `print`s the messages

--- my_logger.py
+++ my_logger.py
@@ -1,35 +1,75 @@
-import threading
-
 from esptool.logger import log, TemplateLogger
 import sys
-import queue
 
-from PySide6.QtCore import QObject, Signal
+_log_queue = None
 
-class _LoggerSignals(QObject):
-    """
-    Class to emit signals to which our GUI can connect slots to lets say update a progress bar
-    """
-    log_emit = Signal(str, tuple, dict)
-    update_progress_bar = Signal(float)
+def set_logger_queue(queue):
+    global _log_queue
+    _log_queue = queue
 
-signal = _LoggerSignals()
+"""
+Format for the communicate queue - 
+{
+    'type':'LOG'/'FINISHED'/'PROGRESS'/'ERROR',
+    'content' : "key for log messages",
+    'payload' : 'final result of the operation , like bytearray',
+    'value' : 'progress values , usually % in float or int'
+    'error' : 'cant connect to board'
+}
+"""
 
 # Dont instantiate manually / more than once
 class CustomLogger(TemplateLogger):
 
-    def print(self, message="", *args, **kwargs):
-        _log_queue.put((message,args,kwargs))
-        # signal.log_emit.emit(message, args, kwargs)
+    def print(self, message, *args, **kwargs):
+        if _log_queue is None:
+            print("NOLOGQ: ", message, args, kwargs)
+            return
+
+        if isinstance(message, dict):
+            _log_queue.put(message)
+        else:
+            for arg in args:
+                message += str(arg)
+            _log_queue.put(
+                {
+                    'type':'LOG',
+                    'content': message
+                }
+            )
 
     def note(self, message):
-        self.print(f"NOTE: {message}")
+        if _log_queue is None:
+            self.print(f" {message}")
+        else:
+            self.print(
+                {
+                    'type': 'LOG',
+                    'content': message
+                }
+            )
 
     def warning(self, message):
-        self.print(f"WARNING: {message}")
+        if _log_queue is None:
+            self.print(f" {message}")
+        else:
+            self.print(
+                {
+                    'type': 'LOG',
+                    'content': message
+                }
+            )
 
     def error(self, message):
-        self.print(message, file=sys.stderr)
+        if _log_queue is None:
+            self.print(f" {message}")
+        else:
+            self.print(
+                {
+                    'type': 'ERROR',
+                    'error': message
+                }
+            )
 
     def stage(self, finish=False):
         # Collapsible stages not needed in this example
@@ -43,29 +83,26 @@ class CustomLogger(TemplateLogger):
             suffix = "",
             bar_length: int = 30,
     ):
-        # Progress bars replaced with simple percentage output in this example
-        percent = 100 * (cur_iter / float(total_iters))
-        # signal.update_progress_bar.emit(percent)
-        percent_string = f"{percent:.1f}"
-        if percent >= 100:
-            self.print(f"Finished: {percent_string}%")
+        # Calculate percentage of progress
+        percent = int(100 * (cur_iter / float(total_iters)))
+        if _log_queue is None:
+            self.print(f" {percent}%")
         else:
-            self.print(f"{prefix} {suffix} {percent_string}%")
+            self.print(
+                {
+                    'type': 'PROGRESS',
+                    'value': percent
+                }
+            )
+
 
     def set_verbosity(self, verbosity):
         # Set verbosity level not needed in this example
         pass
 
-_log_queue = queue.Queue()
 
-def _printer_thread():
-    while True:
-        message, args, kwargs = _log_queue.get()
-        print(f"{message}", *args, **kwargs)
 
-_printer_worker = threading.Thread(target=_printer_thread, daemon=True)
-_printer_worker.start()
-print("logging worker thread started")
+
 
 # Replace the default logger with the custom logger
 log.set_logger(CustomLogger())
